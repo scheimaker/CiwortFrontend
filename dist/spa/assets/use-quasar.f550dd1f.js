a0b29b356b74f0c407aba61180a8e2a3b63b807a
Sicheng Zhu <sczhu1990@hotmail.com>
2025-01-17 13:55:57 +0100

    submit for successful build

---
+++ dist/spa/assets/use-quasar.f550dd1f.js
@@ -0,0 +1,1 @@
+import{i as a,$ as r}from"./index.0fcb0f68.js";function u(){return a(r)}export{u};
--- public/dist/spa/assets/use-quasar.4d06c9b9.js
+++
@@ -1,1 +0,0 @@
-import{i as a,$ as r}from"./index.6cc5e6e2.js";function u(){return a(r)}export{u};
